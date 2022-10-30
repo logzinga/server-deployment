aba9634091be3eaaddc63e0d003905ad6ac02526
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 15:51:03 +1100

    its almost complete however i require sudo

--- setup2.sh
+++ setup2.sh
@@ -4,9 +4,6 @@ systemctl enable NetworkManager
 systemctl enable sshd
 cd /
 git clone https://github.com/logzinga/server-deployment
-cd server-deployment
-git checkout vm
-cd ..
 
 
 ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
@@ -27,6 +24,8 @@ passwd server
 
 echo "Root Password"
 passwd
+
+rm -R /server-deployment
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
