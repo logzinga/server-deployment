d9e635ecf4076ede79d312f5ecd6f8b468e16421
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 14:55:25 +1100

    bruh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ parted /dev/vda set 1 esp on
 parted /dev/vda mkpart "swap" linux-swap 301MiB 16GiB
 parted /dev/vda mkpart "root" ext4 16GiB 100%
 
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/vda3
+mkswap /dev/vda2
+mkfs.fat -F 32 /dev/vda1
 
 mount /dev/sda3 /mnt
-swapon /dev/sda2 
-mount --mkdir /dev/sda1 /mnt/boot
+swapon /dev/vda2 
+mount --mkdir /dev/vda1 /mnt/boot
 
 pacstrap -K /mnt base linux linux-firmware openssh networkmanager nano grub efibootmgr
 
