04f23bd0db75e7d75163093358bdf99501235fbc
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 14:21:13 +1100

    got arch to setup normally in first script

--- setup.sh
+++ setup.sh
@@ -9,4 +9,22 @@ parted /dev/sda set 1 esp on
 parted /dev/sda mkpart "swap" linux-swap 301MiB 16GiB
 parted /dev/sda mkpart "root" ext4 16GiB 100%
 
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+mkfs.fat -F 32 /dev/sda1
+
+mount /dev/sda3 /mnt
+swapon /dev/sda2 
+mount --mkdir /dev/sda1 /mnt/boot
+
+pacstrap -K /mnt base linux linux-firmware openssh networkmanager nano
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp setup2.sh /mnt/usr/bin/setup
+chmod +777 /mnt/usr/bin/setup
+
+echo "When ready, type 'setup' to continue"
+arch-chroot /mnt
+
 
