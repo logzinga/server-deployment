e53cc3958901bf9ec7eeb87356504b42dfc7ba8b
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 15:21:20 +1100

    testing support for script flags...

--- setup.sh
+++ setup.sh
@@ -3,7 +3,14 @@
 echo "What do you want to name this server? (no special characters)"
 read SERVERNAME
 
-parted /dev/vda mklabel gpt
+while getopts 'lh:' OPTION; do
+  case "$OPTION" in
+    l)
+      echo "Setting up for Desktop..."
+      ;;
+    h)
+      echo "Setting up for VM..."
+      parted /dev/vda mklabel gpt
 parted /dev/vda mkpart "EFI" fat32 1MiB 301MiB
 parted /dev/vda set 1 esp on
 parted /dev/vda mkpart "swap" linux-swap 301MiB 16GiB
@@ -28,5 +35,11 @@ echo $SERVERNAME >> /mnt/etc/hostname
 
 echo "When ready, type 'setup' to continue"
 arch-chroot /mnt
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
 
 
