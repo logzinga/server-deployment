f128c38b8fb70de50a2c39b19844d0d0acc47cc4
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 14:06:01 +1100

    added partitioning

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@
 echo "What do you want to name this server? (no special characters)"
 read SERVERNAME
 
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart "EFI" fat32 1MiB 301MiB
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart "swap" linux-swap 301MiB 16GiB
+parted /dev/sda mkpart "root" ext4 16GiB 100%
+
+
