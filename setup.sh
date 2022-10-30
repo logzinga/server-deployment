f6a818e388295476d116e8bf9867962a549bba7d
logzinga <65839350+logzinga@users.noreply.github.com>
2022-10-30 15:23:27 +1100

    more support

--- setup.sh
+++ setup.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-echo "What do you want to name this server? (no special characters)"
-read SERVERNAME
+
 
 while getopts 'lh:' OPTION; do
   case "$OPTION" in
     l)
       echo "Setting up for Desktop..."
+      echo "What do you want to name this server? (no special characters)"
+read SERVERNAME
+
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart "EFI" fat32 1MiB 301MiB
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart "swap" linux-swap 301MiB 16GiB
+parted /dev/sda mkpart "root" ext4 16GiB 100%
+
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+mkfs.fat -F 32 /dev/sda1
+
+mount /dev/sda3 /mnt
+swapon /dev/sda2 
+mount --mkdir /dev/sda1 /mnt/boot
+
+pacstrap -K /mnt base linux linux-firmware openssh networkmanager nano grub efibootmgr
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp setup2.sh /mnt/usr/bin/setup
+chmod +777 /mnt/usr/bin/setup
+
+echo $SERVERNAME >> /mnt/etc/hostname 
+
+echo "When ready, type 'setup' to continue"
+arch-chroot /mnt
+
       ;;
     h)
       echo "Setting up for VM..."
+      echo "What do you want to name this server? (no special characters)"
+read SERVERNAME
       parted /dev/vda mklabel gpt
 parted /dev/vda mkpart "EFI" fat32 1MiB 301MiB
 parted /dev/vda set 1 esp on
